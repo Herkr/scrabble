291ff612c1dd3cb3ccce27200696b62b2c2b7b8a
Herborg <Herborg@LAPTOP-5O84QV7D>
2019-11-05 14:50:04 +0000

    Updates

---
+++ precache-manifest.ec58d7d7518ee4af3907e610223d30a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b1a306cbb1d324b7dcb1fdddf6f25b",
+    "url": "/scrabble/index.html"
+  },
+  {
+    "revision": "8daba5c9263ab18deb5b",
+    "url": "/scrabble/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "473beaba07be9cb049ea",
+    "url": "/scrabble/static/js/2.0008e5e5.chunk.js"
+  },
+  {
+    "revision": "8daba5c9263ab18deb5b",
+    "url": "/scrabble/static/js/main.3a469ae6.chunk.js"
+  },
+  {
+    "revision": "594afff4690c217f8128",
+    "url": "/scrabble/static/js/runtime-main.1041cc1a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/scrabble/static/media/logo.25bf045c.svg"
+  }
+]);
