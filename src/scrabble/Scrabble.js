78684c220621f9a88c3c2f2bb6d3c613423b0726
Herborg <Herborg@LAPTOP-5O84QV7D>
2019-11-07 20:49:41 +0000

    design alert

--- src/scrabble/Scrabble.js
+++ src/scrabble/Scrabble.js
@@ -47,12 +47,12 @@ function Scrabble() {
 
           <h2>{e => setRandomWord(e.target.value)}</h2>
           <h2>{e => setRandomLetters(e.target.value)}</h2>
-          <h2>{isWordInDirectory(word)}</h2>
           <p>{tiles}</p>
           <h2>Sinasta orð: {word}</h2>
+          <h2>{isWordInDirectory(word)}</h2>
           
           <button className="App-button"><Link to={'/scrabble/toplist'} className="App-link">Eg gevi upp</Link></button>
-          <button className="App-button" onClick={()=>{ alert('found and missing words'); }}>alert</button>
+          <button className="App-button" onClick={()=>{ alert(' missing words'); }}>Onnur orð</button>
           
           <Switch>
             <Route exact path='/scrabble/toplist' component={TopList} />
@@ -69,13 +69,21 @@ function isWordInDirectory(word) {
 
   if(word.length !== 0)
   {
-    for (let i = wordsFromDictionary.length - 1; i > 0; i--) {
+    for (let i = wordsFromDictionary.length - 1; i >= 0; i--) {
       if(wordsFromDictionary[i] === word)
       {
-        return alert('Orðið bleiv funnið!');
+        return (
+          <div class="App-greenAlert">
+            Eitt orð funnið!
+          </div>
+        );
       }
     }
-    return alert('Einki orð funnið');
+    return (
+      <div class="App-redAlert">
+        Einki orð funnið!
+      </div>
+    );
   }
   return;
 }
@@ -97,7 +105,7 @@ function getTiles(shuffledword) {
 
 // Richard Durstenfeld shuffle algorithm
 function shuffleArray(array) {
-  for(let i = array.length - 1; i > 0; i--) {
+  for(let i = array.length - 1; i >= 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     let temp = array[i];
     array[i] = array[j];
@@ -132,7 +140,7 @@ function getShuffledWordFromDictionary(lengthOfWord) {
   // unicode: Á=\u00C1 Ð=\u00D0 Í=\u00CD Ó=\u00D3 Ú=\u00DA Ý=\u00DD Æ=\u00C6 Ø=\u00D8'   
   var checkChar = /^[A-Z\u00C1\u00D0\u00CD\u00D3\u00DA\u00DD\u00C6\u00D8]/;
 
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
+  for (let i = shuffledArray.length - 1; i >= 0; i--) {
     // check if upper case
     if(checkChar.test(shuffledArray[i]) === false){
       items.push(shuffledArray[i]);
