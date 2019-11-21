51edd9e7de0ff286ff8a5cf611dfe759a9b39721
Herborg <Herborg@LAPTOP-5O84QV7D>
2019-11-21 02:20:42 +0000

    Updates

---
+++ precache-manifest.7913ce9d73b7fc07b4c0ffaa4da0e364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04ad5322a02dbec62d573345ce18e7c",
+    "url": "/scrabble/index.html"
+  },
+  {
+    "revision": "1a13e7767349ef420b70",
+    "url": "/scrabble/static/css/main.60332b69.chunk.css"
+  },
+  {
+    "revision": "cbaa934c499f8ebec44b",
+    "url": "/scrabble/static/js/2.b3edd854.chunk.js"
+  },
+  {
+    "revision": "1a13e7767349ef420b70",
+    "url": "/scrabble/static/js/main.4a909e6f.chunk.js"
+  },
+  {
+    "revision": "594afff4690c217f8128",
+    "url": "/scrabble/static/js/runtime-main.1041cc1a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scrabble/precache-manifest.91e41d1bd4f64319fe6666b7e7bc16de.js"
+  "/scrabble/precache-manifest.7913ce9d73b7fc07b4c0ffaa4da0e364.js"
 );
 
 self.addEventListener('message', (event) => {
